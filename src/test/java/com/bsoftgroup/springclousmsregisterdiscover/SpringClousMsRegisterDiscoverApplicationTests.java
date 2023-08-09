85b4f5e69f6c30ee6a32edb5a703d661c5acb0ba
Junior Guerra <ext_juguerrac@falabella.cl>
2023-08-09 08:58:19 -0500

    first commit

---
+++ src/test/java/com/bsoftgroup/springclousmsregisterdiscover/SpringClousMsRegisterDiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsoftgroup.springclousmsregisterdiscover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClousMsRegisterDiscoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
